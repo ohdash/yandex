d90b4a44df17006b1c187d704047209635d7baa6
ohdash <87712835+ohdash@users.noreply.github.com>
2022-07-17 00:20:28 +0300

    condi solved

--- condi.py
+++ condi.py
@@ -4,29 +4,37 @@ input.close()
 
 t=[]
 s = lines[0].split()
-mode = lines[1]	
-print ('mode', mode)
+mode = lines[1].split()
+print ('mode', mode[0])
+print(type(mode[0]))
+print (len(mode[0]))
+print (mode[0]=='heat')
+
 
 for i in s:
     t.append(int(i))
-if mode not in ('freeze', 'heat', 'auto', 'fan'):
+
+a=''
+
+if mode[0] not in ('freeze', 'heat', 'auto', 'fan'):
 	a=''
 	print('a0 ', a)
-elif mode == 'freeze':
+elif mode[0] == 'freeze':
 	a= str(min(t[0], t[1]))
 	print('a1 ', a)
-elif mode == 'heat':
+elif mode[0] == 'heat':
 	a=str(max(t[0], t[1]))
 	print('a2 ', a)
-elif mode == 'fan':
+elif mode[0] == 'fan':
 	a=str(t[0])
 	print('a3 ', a)
-elif mode == 'auto':
+elif mode[0] == 'auto':
 	a=str(t[1])
 	print('a4 ', a)
 
 if abs(t[0]) > 50 or abs(t[1]) > 50 :
 	a=''
+
 print(a)
 print(t)
 print(mode)
